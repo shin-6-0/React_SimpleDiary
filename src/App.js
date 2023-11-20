3d0c556f6b6a80057038c8498b140e33255bb9b5
shin-6-0 <syk6715@naver.com>
2023-11-20 15:11:43 +0900

    배열 사용하기 - 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList=[
+  {
+    id:1,
+    author:"신유경",
+    content:"일기작성중!",
+    emotion:5,
+    created_date:new Date().getTime()
+    //Date의 getTime > 시간 반환
+  },
+  {
+    id:2,
+    author:"춘식",
+    content:"쭝시기의 일기장",
+    emotion:3,
+    created_date:new Date().getTime()
+  },
+  {    id:3,
+    author:"프로도",
+    content:"리액트 어렵다",
+    emotion:2,
+    created_date:new Date().getTime()}
+]
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
